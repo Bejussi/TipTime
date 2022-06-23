a0a80b65e3cee383517b4d565b5f661a41c9108e
Alexandra <pindooqy@gmail.com>
2022-06-23 19:15:08 +0300

    Change design of main screen, add calculation of total and per person money

--- app/src/main/java/com/bejussi/tiptime/MainActivity.kt
+++ app/src/main/java/com/bejussi/tiptime/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.bejussi.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.bejussi.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,28 +21,68 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.calculateButton.setOnClickListener { calculateTip() }
+
+        binding.costOfService.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(view, keyCode)
+        }
     }
 
-    private fun calculateTip() {
-        val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDoubleOrNull()
-        if (cost == null) {
-            binding.tipResult.text = ""
-            return
+        private fun calculateTip() {
+            val stringCostInTextField = binding.costOfService.text.toString()
+            val cost =  stringCostInTextField.toDoubleOrNull()
+            if (cost == null) {
+                binding.tipResult.text = getString(R.string.null_text)
+                binding.totalResult.text = getString(R.string.null_text)
+                binding.perPersonResult.text = getString(R.string.null_text)
+                return
+            }
+
+            val stringPersonInTextField = binding.numberOfPeople.text.toString()
+            val person = stringPersonInTextField.toDoubleOrNull()
+            if(person == null) {
+
+            }
+
+
+            val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+                R.id.option_zero_percent -> 0.0
+                R.id.option_ten_percent -> 0.1
+                R.id.option_fifteen_percent -> 0.15
+                else -> 0.2
+            }
+
+            var tip = tipPercentage * cost
+            if (binding.roundUpSwitch.isChecked) {
+                tip = kotlin.math.ceil(tip)
+            }
+
+            //val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+            binding.tipResult.text = formatPrice(tip)
+
+            val total = cost + tip
+            //val formattedTotal = NumberFormat.getCurrencyInstance().format(total)
+            binding.totalResult.text = formatPrice(total)
+
+            val perPerson = total / person!!
+            //val formattedRerPerson = NumberFormat.getCurrencyInstance().format(perPerson)
+            binding.perPersonResult.text = formatPrice(perPerson)
+
         }
 
-        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
-            R.id.option_twenty_percent -> 0.20
-            R.id.option_eighteen_percent -> 0.18
-            else -> 0.15
+        private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+            if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                // Hide the keyboard
+                val inputMethodManager =
+                    getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+                return true
+            }
+            return false
         }
 
-        var tip = tipPercentage * cost
-        if (binding.roundUpSwitch.isChecked) {
-            tip = kotlin.math.ceil(tip)
-        }
-
-        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    private fun formatPrice(price: Double): String {
+        return "%.1f".format(price)
     }
 }
+
+
