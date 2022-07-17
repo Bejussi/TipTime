772938e0ee2a7c7a623063142c1aec7059c77c46
Alexandra <pindooqy@gmail.com>
2022-07-17 20:42:17 +0300

    Add ViewModel and DataBinding

--- app/src/main/java/com/bejussi/tiptime/MainActivity.kt
+++ app/src/main/java/com/bejussi/tiptime/MainActivity.kt
@@ -3,85 +3,75 @@ package com.bejussi.tiptime
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import com.bejussi.tiptime.databinding.ActivityMainBinding
-import java.text.NumberFormat
-import kotlin.math.roundToInt
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val viewModel: TipViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(binding.root)
+
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
 
         binding.calculateButton.setOnClickListener { calculateTip() }
 
         binding.costOfService.setOnKeyListener { view, keyCode, _ ->
             handleKeyEvent(view, keyCode)
         }
+
+        binding.numberOfPeople.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(view, keyCode)
+        }
     }
 
-        private fun calculateTip() {
-            val stringCostInTextField = binding.costOfService.text.toString()
-            val cost =  stringCostInTextField.toDoubleOrNull()
-            if (cost == null) {
-                binding.tipResult.text = getString(R.string.null_text)
-                binding.totalResult.text = getString(R.string.null_text)
-                binding.perPersonResult.text = getString(R.string.null_text)
-                return
-            }
+    private fun calculateTip() {
+        val cost = binding.costOfService.text.toString().toDoubleOrNull()
+        val person = binding.numberOfPeople.text.toString().toDoubleOrNull()
 
-            val stringPersonInTextField = binding.numberOfPeople.text.toString()
-            val person = stringPersonInTextField.toDoubleOrNull()
-            if(person == null) {
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_zero_percent -> 0.0
+            R.id.option_ten_percent -> 0.1
+            R.id.option_fifteen_percent -> 0.15
+            else -> 0.2
+        }
 
-            }
-
-
-            val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
-                R.id.option_zero_percent -> 0.0
-                R.id.option_ten_percent -> 0.1
-                R.id.option_fifteen_percent -> 0.15
-                else -> 0.2
-            }
-
-            var tip = tipPercentage * cost
+        if (cost != null && person != null && cost != 0.0 && person != 0.0) {
             if (binding.roundUpSwitch.isChecked) {
-                tip = kotlin.math.ceil(tip)
+                viewModel.roundUpTip(true)
+            } else {
+                viewModel.roundUpTip(false)
             }
+            viewModel.calculateTip(cost,person,tipPercentage)
 
-            //val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-            binding.tipResult.text = formatPrice(tip)
-
-            val total = cost + tip
-            //val formattedTotal = NumberFormat.getCurrencyInstance().format(total)
-            binding.totalResult.text = formatPrice(total)
-
-            val perPerson = total / person!!
-            //val formattedRerPerson = NumberFormat.getCurrencyInstance().format(perPerson)
-            binding.perPersonResult.text = formatPrice(perPerson)
-
+        } else {
+            Toast.makeText(applicationContext, R.string.empty_values, Toast.LENGTH_SHORT).show()
         }
+    }
 
-        private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
-            if (keyCode == KeyEvent.KEYCODE_ENTER) {
-                // Hide the keyboard
-                val inputMethodManager =
-                    getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
-                return true
-            }
-            return false
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
         }
-
-    private fun formatPrice(price: Double): String {
-        return "%.1f".format(price)
+        return false
     }
 }
 
